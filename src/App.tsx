3618796028de42fd044ae66f9940b6ee72f10e6d
dudakp <palodudak@gmail.com>
2021-01-30 16:20:47 +0100

    stage1-tut3.2

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { ReactElement } from 'react';
+import { Grommet, Heading, Main, Paragraph } from 'grommet';
 
-function App() {
+const theme = {
+  global: {
+    font: {
+      family: 'Roboto',
+      size: '14px',
+      height: '20px',
+    },
+  },
+};
+
+export const App = (): ReactElement => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Grommet theme={theme}>
+      <Main pad='large'>
+        <Heading>Hello React</Heading>
+        <Paragraph>We made something</Paragraph>
+      </Main>
+    </Grommet>
   );
-}
+};
 
 export default App;
