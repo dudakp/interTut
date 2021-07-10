65d75e7ca1e8159583df17f22633f987b0d18973
dudakp <palodudak@gmail.com>
2021-07-10 16:29:47 +0200

    added tests

---
+++ src/common/components/darkModeSwitch/DarkModeSwitch.test.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import DarkModeSwitch from './DarkModeSwitch';
+
+it('Component renders', () => {
+  const { container } = render(<DarkModeSwitch />);
+  expect(container.firstChild).toBeInTheDocument();
+});
+
+it('Switch toogle', () => {
+  render(<DarkModeSwitch />);
+
+  const checkbox = screen.getByRole('checkbox');
+  checkbox.click();
+  expect(checkbox).toBeChecked();
+  checkbox.click();
+  expect(checkbox).not.toBeChecked();
+});
